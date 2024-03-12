850fa5170bc6e76fbf9f658da351e0c746a005c8
Julia Kulikova <ciel1863@gmail.com>
2024-03-12 22:21:43 +0300

    fix docker files

--- coordination_centre/coordination_centre/settings.py
+++ coordination_centre/coordination_centre/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.news'
+    'apps.news',
+    'django.contrib.postgres'
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'coordination_centre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
